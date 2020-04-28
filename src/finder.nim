0870eccdfff0c8a19f8fb3d0c2d4a21993af4d99
bung87 <crc32@qq.com>
2020-04-28 13:37:43 +0800

    o

--- src/finder.nim
+++ src/finder.nim
@@ -1,28 +1,27 @@
 
-import os, tables, strformat, base64, ospaths
-import zip/zipfiles
+import os, tables, strformat
+import zip/[zipfiles,libzip],streams
 
-type FinderType {.pure.} = enum
+type FinderType* {.pure.} = enum
   fs,
   fs2mem,
   zip2mem,
   zip
 
-type Finder = object
+type Finder* = object
   case fType:FinderType
     of FinderType.fs:
       base:string
     of FinderType.zip:
-      zipFile:ZipArchive
+      zipFile:ptr ZipArchive
     of FinderType.fs2mem:
       tableData:Table[string,string]
     of FinderType.zip2mem:
-      zipData:string
+      zipData:PZipSource
       
 
-template initFinder(x:typed,arg:typed) =
+template initFinder*(x:typed,arg:typed) =
   block:
-    let isFile =  if p.existsFile: true else : false 
     if x.fType == FinderType.fs2mem:
       # read dir from memory
       var assets = initTable[string, string]()
@@ -36,16 +35,47 @@ template initFinder(x:typed,arg:typed) =
     
     elif x.fType == FinderType.zip2mem:
       # read from memory zip
-      x.zipData = readFile p
+      var err:ptr int32
+      var s = p.cstring
+      x.zipData = zip_source_buffer_create(cast[pointer]( s.unsafeAddr),len(p).uint64,0,err)
     elif x.fType == FinderType.zip:
       # read from file system zip
+      let p = arg.expandTilde.absolutePath
       var zip:ZipArchive
       let openSuccess  = zip.open(p,fmRead)
-      x.zipFile = zip
+      if not openSuccess:
+        raise newException(OSError,fmt"can't open {p}")
+      x.zipFile = zip.addr
+    elif x.fType == FinderType.fs:
+      let p = arg.expandTilde.absolutePath
+      x.base = p
+
+proc get*(x:Finder,path:string):string =
+  if x.fType == FinderType.fs2mem:
+      result = x.tableData[path] 
+  elif x.fType == FinderType.zip2mem:
+    # read from memory zip
+    discard
+  elif x.fType == FinderType.zip:
+    # read from file system zip
+    var s = newStringStream()
+    x.zipFile[].extractFile(path,s)
+    result = s.data
+  elif x.fType == FinderType.fs:
+    let p = absolutePath(path,x.base)
+    result = readFile(p)
 
 when isMainModule:
+  const r = "switch(\"path\", \"$projectDir/../src\")"
   var x:Finder
   x.fType = FinderType.fs2mem
   let p = "./tests"
   initFinder(x,p)
-  echo  x.tableData
+  assert x.tableData.hasKey("config.nims")
+  assert x.get("config.nims") == r
+  var y:Finder
+  y.fType = FinderType.zip
+  let p2 = "./tests/Archive.zip"
+  initFinder(y,p2)
+  assert y.get("config.nims") == r
+    
